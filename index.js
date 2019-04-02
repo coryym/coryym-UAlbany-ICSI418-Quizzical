a7263164de3bd83e80ede880176c327146992c83
sean rowland <seanrowl@buffalo.edu>
2019-04-01 22:16:10 -0400

    added validation for user login need to to admin validation
    These was one of the most boring and tedious thing I have ever done. I'm just glad its done but I can't test it till the database is done.

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+
+const users = require('./routes/api/users.js');
+
+const app = express;
+
+app.use(bodyParser.urlencoded({extened: false}))
+app.use(bodyParser.json())
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -10,14 +19,19 @@ const connection = mysql.createConnection({
 
 connection.connect()
 
+app.use(passport.initialize())
 // make query
-connection.query('', function (err, rows, fields) {
-  if (err){
-    console.error('Error conecting: ' + err.stack);
-  }
+//connection.query('', function (err, rows, fields) {
+//  if (err){
+//    console.error('Error conecting: ' + err.stack);
+//  }
+//
+//  //do stuff if valid
+//})
 
-  //do stuff if valid
-})
+app.use('/api/users', users)
 
+const port = process.env.PORT
+app.listen(port, () => console.log('Server runing on port ' + port))
 //kill connection
 connection.end()
